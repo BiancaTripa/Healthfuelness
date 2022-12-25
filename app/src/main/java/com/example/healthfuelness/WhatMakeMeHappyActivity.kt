b9db92fa72d09850326e7af23677c6f2ae2f46b4
CristinaUrucu <77269897+CristinaUrucu@users.noreply.github.com>
2022-12-25 13:47:05 +0200

    Go from home page to what make me happy and back

--- app/src/main/java/com/example/healthfuelness/WhatMakeMeHappyActivity.kt
+++ app/src/main/java/com/example/healthfuelness/WhatMakeMeHappyActivity.kt
@@ -6,23 +6,16 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 
-class HomeActivity : AppCompatActivity() {
+class WhatMakeMeHappyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_what_make_me_happy)
 
         val logoutNowButton = findViewById<TextView>(R.id.button_logout)
-        val whatMakeMeHappyButton = findViewById<ImageView>(R.id.button_happy)
         val homePageButton = findViewById<ImageView>(R.id.button_home)
-
 
         logoutNowButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-        whatMakeMeHappyButton.setOnClickListener {
-            val intent = Intent(this, WhatMakeMeHappyActivity::class.java)
             startActivity(intent)
         }
 
