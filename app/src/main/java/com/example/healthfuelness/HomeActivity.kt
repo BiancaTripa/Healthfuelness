f9a9edcd0975e989ffccce608280e1bcdff9371b
CristinaUrucu <77269897+CristinaUrucu@users.noreply.github.com>
2022-12-25 14:20:23 +0200

    random quote from real time database is generated every time you go to homepage

--- app/src/main/java/com/example/healthfuelness/HomeActivity.kt
+++ app/src/main/java/com/example/healthfuelness/HomeActivity.kt
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeActivity : AppCompatActivity() {
+
+    //create object of DatabaseReference class to access firebase's Realtime Database
+    private val databaseReference =  FirebaseDatabase.getInstance().getReferenceFromUrl("https://healthfuelness-d8e8a-default-rtdb.firebaseio.com/")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -14,6 +23,32 @@ class HomeActivity : AppCompatActivity() {
         val logoutNowButton = findViewById<TextView>(R.id.button_logout)
         val whatMakeMeHappyButton = findViewById<ImageView>(R.id.button_happy)
         val homePageButton = findViewById<ImageView>(R.id.button_home)
+        val quoteText = findViewById<TextView>(R.id.tv_daily_quote)
+
+        val getContext = this
+        databaseReference.child("quote").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val randomId = (1 until 11).random()
+
+                //check if quote is exist in firebase database
+                if (snapshot.hasChild(randomId.toString())) {
+
+                    val getQuote = snapshot.child(randomId.toString()).getValue(String::class.java)
+                    quoteText.text = getQuote
+
+                } else {
+                    quoteText.text = "Some went wrong with quote"
+                    Toast.makeText(getContext, "Wrong quote id", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("The read failed: " + error.code);
+            }
+        })
 
 
         logoutNowButton.setOnClickListener {
