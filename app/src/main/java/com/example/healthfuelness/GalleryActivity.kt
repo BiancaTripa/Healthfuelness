934f542e42d3a9227736d93b3c990161958dbe62
CristinaUrucu <77269897+CristinaUrucu@users.noreply.github.com>
2023-01-09 22:28:55 +0200

    button to gallery and random quote

---
+++ app/src/main/java/com/example/healthfuelness/GalleryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.healthfuelness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
