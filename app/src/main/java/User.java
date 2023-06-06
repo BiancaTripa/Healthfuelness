db13cddf06ec133ca90d49de0c7462dbe92b7c3d
BiancaTripa <80540181+BiancaTripa@users.noreply.github.com>
2023-06-06 15:40:55 +0300

    user

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,27 +1,52 @@
+package com.example.healthfuelness;
+
 public class User {
     private String fullName, email, password;
-    private static int age;
-    private static int height;
-    private static int weight;
-    private static String date;
+    private static String date, date1, date2, date3, date4;
     private static String username;
     private static int currentDateOrNot; // -1: previous; 0: current; 1: future
 
-    public User(String fullName, int age, int height, int weight, String email, String password) {
+    public User(String fullName, String email, String password) {
         this.email = email;
         this.fullName = fullName;
         this.password = password;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
     }
 
     public static void setDate(String date) {
         User.date = date;
     }
-
     public static String getDate() {
         return date;
+    }
+
+    public static void setDate1(String date) {
+        User.date1 = date;
+    }
+    public static String getDate1() {
+        return date1;
+    }
+
+    public static void setDate2(String date) {
+        User.date2 = date;
+    }
+    public static String getDate2() {
+        return date2;
+    }
+
+    public static void setDate3(String date) {
+        User.date3 = date;
+    }
+
+    public static String getDate3() {
+        return date3;
+    }
+
+    public static void setDate4(String date) {
+        User.date4 = date;
+    }
+
+    public static String getDate4() {
+        return date4;
     }
 
     public static void setUsername(String username) {
@@ -31,30 +56,6 @@ public class User {
     public static String getUsername() {
         return username;
     }
-
-    //public void setAge(int age) {
-       // User.age = age;
-   // }
-
-   // public static int getAge() {
-      //  return age;
-   // }
-
-    //public void setHeight(int height) {
-      //  User.height = height;
-    //}
-
-   // public static int getHeight() {
-       // return height;
-    //}
-
-   // public void setWeight(int weight) {
-        //User.weight = weight;
-   // }
-
-   // public static int getWeight() {
-        //return weight;
-    //}
 
     public static void setCurrentDateOrNot(int currentDateOrNot) {
         User.currentDateOrNot = currentDateOrNot;
